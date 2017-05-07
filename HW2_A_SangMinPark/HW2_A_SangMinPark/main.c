ad3f4f31fa1d5f80db605bd1ffd83d227238db80
SangMinPark <psm9733@naver.com>
2017-05-07 14:30:09 +0900

    delete HW2_A_SangMinPark

--- HW2_A_SangMinPark/HW2_A_SangMinPark/main.c
+++
@@ -1,9 +0,0 @@
-#include "header.h"
-
-int main() {
-	List *list = (List*)malloc(sizeof(List));
-	list = readData("points.txt");
-	list = calcGrade(list);
-	outputResult(list, "grade.txt");
-	return 0;
-}
